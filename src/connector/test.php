70feb3c352d195ba8ca34c307108b5cc13d9dd12
SolvingStorm <robo84@gmail.com>
2017-04-12 21:57:32 +0200

    clean-up API test

--- src/connector/test.php
+++ src/connector/test.php
@@ -1,27 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
 
-// include 'connect.php';
-// mysqli_select_db($con, 'dbnaam') or die('Could not select db');
+    $outp = '[{"ID": "1", "Description":"This is record number 1"},{"ID": "2", "Description":"This is record number 2"},{"ID": "3", "Description":"This is record number 3"},{"ID": "4", "Description":"This is record number 4"},{"ID": "5", "Description":"This is record number 5"},{"ID": "6", "Description":"This is record number 6"},{"ID": "7", "Description":"This is record number 7"},{"ID": "8", "Description":"This is record number 8"},{"ID": "9", "Description":"This is record number 9"},{"ID": "10", "Description":"This is record number 10"}]'; 
 
-// $result = $con->query("SELECT ID, ListID, Description, Checked, DateAdded, DateChecked FROM groceries WHERE Checked = 0");
-
-// $outp = "[";
-// while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-//     if ($outp != "[") {$outp .= ",";}
-//     $outp .= '{"ID":"'  . $rs["ID"] . '",';
-//     $outp .= '"ListID":"'   . $rs["ListID"]        . '",';
-// 	$outp .= '"Description":"'   . $rs["Description"]        . '",';
-// 	$outp .= '"Checked":"'   . $rs["Checked"]        . '",';
-// 	$outp .= '"DateAdded":"'   . $rs["DateAdded"]        . '",';
-//     $outp .= '"DateChecked":"'. $rs["DateChecked"]     . '"}'; 
-// }
-// $outp .="]";
-
-//$con->close();
-
-$outp = '[{"ID": "1", "Description":"This is record number 1"},{"ID": "2", "Description":"This is record number 2"},{"ID": "3", "Description":"This is record number 3"},{"ID": "4", "Description":"This is record number 4"},{"ID": "5", "Description":"This is record number 5"}]';
-
-echo($outp);
+    echo($outp);
 ?>
